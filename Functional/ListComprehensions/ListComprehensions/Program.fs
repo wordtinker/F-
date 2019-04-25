c9373629288274170bdcc961ca789606f25914f0
wordtinker <wordtinker@yandex.ru>
2019-04-25 15:48:17 +0300

    .

---
+++ Functional/ListComprehensions/ListComprehensions/Program.fs
@@ -0,0 +1,30 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+// create some list comprehensions
+let numericList = [ 0 .. 9 ]
+let alpherSeq = seq { 'A' .. 'Z' } // IEnumerable
+// create some list comprehensions
+let multiplesOfThree = [ 0 .. 3 .. 30 ]
+let revNumericSeq = [ 9 .. -1 .. 0 ] 
+let squares = seq {for x in 1..10 -> x * x}
+// a sequence of even numbers
+let evens n =
+    seq { for x in 1 .. n do
+            if x % 2 = 0 then yield x } 
+// sequence of tuples representing points
+let squarePoints n =
+    seq { for x in 1 .. n do
+            for y in 1 .. n do
+                yield x, y } 
+[<EntryPoint>]
+let main argv =
+    printfn "%A" numericList
+    printfn "%A" alpherSeq
+    printfn "%A" multiplesOfThree
+    printfn "%A" revNumericSeq
+    printfn "%A" squares
+    printfn "%A" (evens 10) 
+    printfn "%A" (squarePoints 3)
+    0 // return an integer exit code
---
+++ Functional/Lists/Lists/Program.fs
@@ -0,0 +1,34 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+// lists are immutable
+let emptyList = []
+let oneItem = "one" :: []
+let twoItem = "one" :: "two" :: []
+let shortHand = ["apples"; "pears"]
+let twoLists = oneItem @ twoItem;
+let objList = [box 1; box 2.0; box "three"] // object wrapper
+
+// definition of a concatenation function
+let rec concatList l =
+    match l with
+    | head :: tail -> head @ (concatList tail)
+    | [] -> [] 
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" emptyList
+    printfn "%A" oneItem
+    printfn "%A" twoItem
+    printfn "%A" shortHand
+    printfn "%A" twoLists
+    printfn "%A" objList
+
+    // list to be concatenated
+    let listOfList = [[2; 3; 5]; [7; 11; 13]; [17; 19; 23; 29]]
+    // call the function
+    let primes = concatList listOfList
+    // print the results
+    printfn "%A" primes
+    0 // return an integer exit code
